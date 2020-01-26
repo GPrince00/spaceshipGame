9dc57ea15580a8fcf6c5d83966e8daa984049add
GPrince00 <gustavoprince0@gmail.com>
2020-01-26 20:43:16 -0300

    feat(app.js): created the character

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const ENEMIES_STORE = [];
 const ENEMIES_SIZE = 50;
 const ENEMIES_COLORS = [
-  'red', 'blue', 'yellow', 'white', 'grey',
+  'red', 'blue', 'yellow', 'white',
   'green', 'purple', 'navy', 'silver', 'olive',
   'lime', 'fuchsia', 'teal', 'aqua', 'maroon'
 ];
+const HERO_SIZE = ENEMIES_SIZE;
+const HERO_COLOR = 'grey';
 
 const canvas = document.getElementById('my-canvas');
 const ctx = canvas.getContext('2d');
@@ -28,6 +30,24 @@ class Enemy {
   }
 }
 
+class Hero {
+  constructor() {
+    this.x = 0;
+    this.y = canvas.height - HERO_SIZE;
+    this.width = HERO_SIZE;
+    this.height = HERO_SIZE;
+    this.color = HERO_COLOR;
+  }
+
+  draw() {
+    if (this.x < 0) this.x = 0;
+    if (this.x > canvas.width - HERO_SIZE) this.x = canvas.width - HERO_SIZE;
+
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.x, this.y, HERO_SIZE, HERO_SIZE);  
+  }
+}
+
 const createEnemy = () => {
   if (frames % 5 === 0) {
     const x = Math.floor(Math.random() * 10) * ENEMIES_SIZE;
@@ -39,11 +59,14 @@ const drawEnemies = () => {
   ENEMIES_STORE.forEach(enemy => enemy.draw());
 }
 
+const ourHero = new Hero();
+
 const resetCanvas = () => ctx.clearRect(0, 0, canvas.width, canvas.height);
 
 const render = () => {
   resetCanvas();
   frames += 1;
+  ourHero.draw();
   createEnemy();
   drawEnemies();
   if (running) {
@@ -53,3 +76,14 @@ const render = () => {
 
 running = true;
 render();
+
+window.addEventListener('keydown', (e) => {
+  if (e.keyCode === 37) {
+    if (ourHero.x <= 0) return;
+    ourHero.x -= HERO_SIZE;
+  }
+  if (e.keyCode === 39) {
+    if (ourHero.x >= canvas.width - HERO_SIZE) return;
+    ourHero.x += HERO_SIZE;
+  }    
+});
